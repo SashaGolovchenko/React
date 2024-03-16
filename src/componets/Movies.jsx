a11c7edf70e25ed2f56927c3cdb5e08a2c61e678
SashaGolovchenko <135842969+SashaGolovchenko@users.noreply.github.com>
2024-03-16 18:28:44 +0300

    Update Movies.jsx

--- src/componets/Movies.jsx
+++ src/componets/Movies.jsx
@@ -1,38 +1,20 @@
- import { Movie } from './Movie'
+import { Movie } from './Movie'
 
- function Movies(props) {
-     const { movies = [] } = props;
+function Movies(props) {
+    const { movies = [] } = props;
 
-     return (
-         <div className="movies">
-             {movies.length ? (
-                 movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
-             ) : (
-                 <h4 className='ccc'>Мы не смогли найти фильм по вашему описанию. Проверьте написание фильма</h4>
-             )}
-         </div>
-     );
+    return (
+        <div className="movies">
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <div>
+                    <img className="img_404" src={require('./404.png')} alt="Error 404" />
+                    <p className='ccc'>We couldn't find a movie based on your description. Please check the movie title.</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export{ Movies };
-
-//import { Movie } from './Movie'
-
-//function Movies(props) {
-//    const { movies = [] } = props;
-//
-//    return (
-//        <div className="movies">
-//            {movies.length ? (
-//                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
-//            ) : (
-//                <div>
-//                    <img className="img_404" src={require('./404.png')} alt="Error 404" />
-//                    <p className='ccc'>We couldn't find a movie based on your description. Please check the movie title.</p>
-//                </div>
-//            )}
-//        </div>
-//    );
-//}
-
-//export { Movies };
+export { Movies };
