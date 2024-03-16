e5c5a53c041111b0e8ce0f20eefd958ef34691f3
SashaGolovchenko <golova.a232@gmail.com>
2024-03-16 17:51:34 +0300

    Initial commit

---
+++ src/componets/Movies.jsx
@@ -0,0 +1,38 @@
+// import { Movie } from './Movie'
+
+// function Movies(props) {
+//     const { movies = [] } = props;
+
+//     return (
+//         <div className="movies">
+//             {movies.length ? (
+//                 movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+//             ) : (
+//                 <h4 className='ccc'>Мы не смогли найти фильм по вашему описанию. Проверьте написание фильма</h4>
+//             )}
+//         </div>
+//     );
+// }
+
+// export{ Movies };
+
+import { Movie } from './Movie'
+
+function Movies(props) {
+    const { movies = [] } = props;
+
+    return (
+        <div className="movies">
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <div>
+                    <img className="img_404" src={require('./404.png')} alt="Error 404" />
+                    <p className='ccc'>We couldn't find a movie based on your description. Please check the movie title.</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export { Movies };
